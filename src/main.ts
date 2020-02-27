bb99039c3381b9f7267a9a17d636dabddf1ea84e
Tyler Hogsett <tyhogset@microsoft.com>
2020-02-26 16:14:06 -0800

    stripped down action logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
-import {execFile} from 'child_process'
+//import {execFile} from 'child_process'
 
 async function run(): Promise<void> {
   try {
-    const processPromise = new Promise((resolve, reject) => {
+    /*const processPromise = new Promise((resolve, reject) => {
       const requiredOption: core.InputOptions = {
         required: true
       }
@@ -23,7 +23,16 @@ async function run(): Promise<void> {
       process.addListener('error', error => reject(error.message))
       process.addListener('exit', resolve)
     })
-    await processPromise
+    await processPromise*/
+    const requiredOption: core.InputOptions = {
+      required: true
+    }
+    const zipFile = core.getInput('zipFile', requiredOption)
+    core.info(`zipFile: ${zipFile}`)
+    const packageType = core.getInput('packageType') || 'Both'
+    core.info(`packageType: ${packageType}`)
+    const folder = core.getInput('folder', requiredOption)
+    core.info(`folder: ${folder}`)
   } catch (error) {
     core.setFailed(error.message)
   }
